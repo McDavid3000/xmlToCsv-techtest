fdbb4d807a6244c6e83e1422298502cb1143f74d
McDavid3000 <54386026+McDavid3000@users.noreply.github.com>
2022-10-24 14:20:20 +1300

    Remove deleted file

--- deleted/fileReader.test.js
+++
@@ -1,9 +0,0 @@
-// const assert = require('assert');
-
-// const fileReader = require('../src/fileReader');
-
-// it('Should load a file', async () => {
-//   const fileLoaded = await fileReader('./inputFiles/testfile.xml');
-
-//   assert(fileLoaded);
-// });
